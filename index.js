0645a260d4c4e88e62dd08a11d96fe2c19a64dbc
Onkaggabetse <onkarabetsemale27@icloud.com>
2023-09-15 14:01:44 +0200

    add array-object

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// created Objects with id property
 const arr = [
     { id: 1, name: "Lizzy" },
     { id: 2, name: "Hazel" },
@@ -6,12 +7,21 @@ const arr = [
     { id: 5, name: "Hapiness" },
   
   ];
-  
+  // function will call deleteObjectsById and function takes two parameters,this parameter is expected to be an array of objects.
   function deleteObjectsById(arr, idToDelete) {
+
+    // the filter method in JavaScript,create a new array that contains all elements from the arr array whose id property does not match the idToDelete value.
     return arr.filter(item => item.id !== idToDelete);
   }
-  
+  // This defines a constant variable idToDelete and assigns it the value 3. This is the id value that you want to use as a filter criterion
+  //  to remove objects from the arr.
   const idToDelete = 3;
+
+  // A new constant variable newArray is defined.This variable is assigned the result of calling the deleteObjectsById 
+  // function with two arguments. This argument is the array containing objects that you want to filter and idToDelete is the argument of the id
+  //  value that you want to use as the criterion for filtering.
   const newArray = deleteObjectsById(arr, idToDelete);
   
+  // this will print the filtered array to the console, allowing you to give the result.
   console.log(newArray);
+ 
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// created a constant variable named arrayOfFruits and assigns it an array.
 const arrayOfFruits = [
     "Apple 1",
      "Banana 2 ",
@@ -6,26 +7,32 @@ const arrayOfFruits = [
   ];
   
   function removeNumbers(arr) {
-    const result = [];
+    // Initialize an empty array to store the cleaned strings.
+    const result = []; 
   
     for (let i = 0; i < arr.length; i++) {
-      let cleanedString = arr[i];
-      
+      let cleanedString = arr[i];  // Get the current element from the input array.
+
+      // Remove all numeric digits (0-9) from the string using a regular expression.
       cleanedString = cleanedString.replace(/[0-9]/g, '');
   
+  // Split the string by hyphens ('-') and check if there are multiple parts.
       const parts = cleanedString.split('-');
       if (parts.length > 1) {
+  // If there are multiple parts, take the first part and remove leading spaces.
         cleanedString = parts[0].trim();
       }
   
+  // Add the cleaned string to the result array,removing any leading spaces.
       result.push(cleanedString.trim());
     }
-  
+  // Return the array containing the cleaned strings.
     return result;
   }
   
+  // A new constant variable newArray is declared and assigned the result of calling the removeNumbers 
+  // function with arrayOfFruits as its argument.
   const newArray = removeNumbers(arrayOfFruits);
-  
   
   console.log(newArray);
   
